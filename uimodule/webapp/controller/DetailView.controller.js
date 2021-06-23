5fda718ba555973cf7f2817403ee723e1394c557
Bhaskar Dhara <dharabhaskar@gmail.com>
2021-06-23 08:07:47 +0530

    The previous positions dialogue issue fixed.

--- uimodule/webapp/controller/DetailView.controller.js
+++ uimodule/webapp/controller/DetailView.controller.js
@@ -13,19 +13,81 @@ sap.ui.define([
 ], function(Controller, JSONModel, Filter, FilterOperator, MessageToast, MessageBox, Fragment, Button, Dialog, List, StandardListItem) {
 	"use strict";
 	return Controller.extend("com.infocus.PMSApproval.controller.DetailView", {
-		onInit:function(){
-			var eventBus=sap.ui.getCore().getEventBus();
-			eventBus.subscribe("DetailView","ShowDetailView",this.onShowDetailView,this);
+		onInit: function() {
+			var eventBus = sap.ui.getCore().getEventBus();
+			eventBus.subscribe("DetailView", "ShowDetailView", this.onShowDetailView, this);
 		},
-		onDesignationPress:function(){
+		onDesignationPress: function() {
 			console.log('Designation press called...');
 		},
-		onShowDetailView:function(source,event,data){
-			MessageBox.show("Showing data for:"+data.empName)
+		onShowDetailView: function(source, event, data) {
+			MessageBox.show("Showing data for:" + data.empName)
 		},
-		onExit:function(){
-			var eventBus=sap.ui.getCore().getEventBus();
-			eventBus.unsubscribe("DetailView","ShowDetailView",this.onShowDetailView,this);
+		onDesignationPress: function() {
+			console.log(this.getView().getModel('posititons'))
+
+			var that = this;
+			if (!that.resizableDialog) {
+				var oTable = new sap.m.Table("tab-1", {
+					inset: true,
+					mode: sap.m.ListMode.None,
+					includeItemInSelection: false,
+				});
+				var col1 = new sap.m.Column("col1", {
+					header: new sap.m.Label({
+						text: "Position"
+					})
+				});
+				var col2 = new sap.m.Column("col2", {
+					header: new sap.m.Label({
+						text: "Period"
+					})
+				});
+				var col3 = new sap.m.Column("col3", {
+					header: new sap.m.Label({
+						text: "Location"
+					})
+				});
+
+				oTable.bindItems("positions>/results", new sap.m.ColumnListItem({
+					cells: [new sap.m.Text({
+						text: "{positions>Position}"
+					}), new sap.m.Text({
+						text: "{positions>Period}"
+					}), new sap.m.Text({
+						text: "{positions>Location}",
+					}), ]
+				}));
+
+				oTable.addColumn(col1);
+				oTable.addColumn(col2);
+				oTable.addColumn(col3);
+
+				that.resizableDialog = new Dialog({
+					title: 'Details of last three position (Excluding Present)',
+					contentWidth: "650px",
+					contentHeight: "200px",
+					resizable: true,
+					content: oTable,
+					beginButton: new Button({
+						text: 'Close',
+						press: function() {
+							that.resizableDialog.close();
+						}
+					})
+				});
+
+				//to get access to the global model
+				this.getView().addDependent(that.resizableDialog);
+			}
+
+			that.resizableDialog.open();
+
+			//this._getDialog().open();
+		},
+		onExit: function() {
+			var eventBus = sap.ui.getCore().getEventBus();
+			eventBus.unsubscribe("DetailView", "ShowDetailView", this.onShowDetailView, this);
 		}
 	});
 });
--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -226,68 +226,6 @@ sap.ui.define([
 
 			//MessageToast.show(aContexts[0].sPath);
 		},
-		onDesignationPress: function() {
-			console.log(this.getView().getModel('posititons'))
-
-			var that = this;
-			if (!that.resizableDialog) {
-				var oTable = new sap.m.Table("tab-1", {
-					inset: true,
-					mode: sap.m.ListMode.None,
-					includeItemInSelection: false,
-				});
-				var col1 = new sap.m.Column("col1", {
-					header: new sap.m.Label({
-						text: "Position"
-					})
-				});
-				var col2 = new sap.m.Column("col2", {
-					header: new sap.m.Label({
-						text: "Period"
-					})
-				});
-				var col3 = new sap.m.Column("col3", {
-					header: new sap.m.Label({
-						text: "Location"
-					})
-				});
-
-				oTable.bindItems("positions>/results", new sap.m.ColumnListItem({
-					cells: [new sap.m.Text({
-						text: "{positions>Position}"
-					}), new sap.m.Text({
-						text: "{positions>Period}"
-					}), new sap.m.Text({
-						text: "{positions>Location}",
-					}), ]
-				}));
-
-				oTable.addColumn(col1);
-				oTable.addColumn(col2);
-				oTable.addColumn(col3);
-
-				that.resizableDialog = new Dialog({
-					title: 'Details of last three position (Excluding Present)',
-					contentWidth: "650px",
-					contentHeight: "200px",
-					resizable: true,
-					content: oTable,
-					beginButton: new Button({
-						text: 'Close',
-						press: function() {
-							that.resizableDialog.close();
-						}
-					})
-				});
-
-				//to get access to the global model
-				this.getView().addDependent(that.resizableDialog);
-			}
-
-			that.resizableDialog.open();
-
-			//this._getDialog().open();
-		},
 		formatAllEmpData: function(emp) {
 			var _self = this;
 			this.empDetails = emp;
